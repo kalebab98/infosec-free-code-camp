77a3467713e8106db1a1a4abfca4ec2c01e540ff
kalebab98 <ethiothend@gmail.com>
2025-06-30 00:41:11 +0300

    final

--- myApp.js
+++ myApp.js
@@ -2,13 +2,9 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
-const PORT = process.env.PORT || 3030;
+
 
 // your code
-
-app.listen(PORT, () => {
- 
-});
 
 
 
